48986559d5562e791c74fcaef42e191e5750915a
Stephen Carman <shcarman@gmail.com>
2022-05-06 12:27:31 -0400

    Updated build script to use windows crate instead of winapi

--- build.rs
+++ build.rs
@@ -1,78 +1,58 @@
-use std::{env, fs, ptr, slice, str};
+use std::error::Error;
+use std::ptr::null;
+use std::{env, fs, slice, str};
 
-use winapi::{shared::winerror::S_OK, um::d3dcommon::ID3DBlob, um::d3dcompiler::D3DCompile};
+use windows::Win32::Graphics::Direct3D::Fxc::D3DCompile;
+use windows::Win32::Graphics::Direct3D::ID3DBlob;
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error + 'static>> {
     static VERTEX_SHADER: &str = include_str!("src/vertex_shader.vs_4_0");
     static PIXEL_SHADER: &str = include_str!("src/pixel_shader.ps_4_0");
 
-    let mut err = ptr::null_mut();
+    let mut err = None; // Never used, but left in-case inspection later is needed
+    let mut vs_blob = None;
+    let mut ps_blob = None;
 
     unsafe {
-        let mut vs_blob = ptr::null_mut();
-        if D3DCompile(
-            VERTEX_SHADER.as_ptr().cast(),
+        D3DCompile(
+            VERTEX_SHADER.as_ptr() as _,
             VERTEX_SHADER.len(),
-            ptr::null_mut(),
-            ptr::null_mut(),
-            ptr::null_mut(),
-            "main\0".as_ptr().cast(),
-            "vs_4_0\0".as_ptr().cast(),
+            None,
+            null(),
+            None,
+            "main\0",
+            "vs_4_0\0",
             0,
             0,
             &mut vs_blob,
             &mut err,
-        ) != S_OK
-        {
-            report_err(err)
-        }
+        )?;
         if let Some(vs_blob) = vs_blob.as_ref() {
-            write_blob("vertex_shader.vs_4_0", vs_blob);
+            write_blob("vertex_shader.vs_4_0", vs_blob)?;
         }
-    }
-    unsafe {
-        let mut ps_blob = ptr::null_mut();
-        if D3DCompile(
-            PIXEL_SHADER.as_ptr().cast(),
+
+        D3DCompile(
+            PIXEL_SHADER.as_ptr() as _,
             PIXEL_SHADER.len(),
-            ptr::null_mut(),
-            ptr::null_mut(),
-            ptr::null_mut(),
-            "main\0".as_ptr().cast(),
-            "ps_4_0\0".as_ptr().cast(),
+            None,
+            null(),
+            None,
+            "main\0",
+            "ps_4_0\0",
             0,
             0,
             &mut ps_blob,
             &mut err,
-        ) != S_OK
-        {
-            report_err(err)
-        }
+        )?;
         if let Some(ps_blob) = ps_blob.as_ref() {
-            write_blob("pixel_shader.ps_4_0", ps_blob);
+            write_blob("pixel_shader.ps_4_0", ps_blob)?;
         }
     }
+    Ok(())
 }
 
-unsafe fn write_blob(shader_name: &str, blob: &ID3DBlob) {
-    let out_dir = env::var("OUT_DIR").unwrap();
+unsafe fn write_blob(shader_name: &str, blob: &ID3DBlob) -> Result<(), Box<dyn Error + 'static>> {
+    let out_dir = env::var("OUT_DIR")?;
     let data = slice::from_raw_parts(blob.GetBufferPointer().cast::<u8>(), blob.GetBufferSize());
-    let _ = fs::write(&format!("{}/{}", out_dir, shader_name), data)
-        .map_err(|e| panic!("Unable to write {} shader to out dir: {:?}", shader_name, e));
-    blob.Release();
-}
-
-unsafe fn report_err(err: *const ID3DBlob) -> ! {
-    let err_msg = err
-        .as_ref()
-        .and_then(|err| {
-            str::from_utf8(slice::from_raw_parts(
-                err.GetBufferPointer().cast::<u8>(),
-                err.GetBufferSize(),
-            ))
-            .ok()
-        })
-        .map(ToOwned::to_owned);
-    err.as_ref().map(|err| err.Release());
-    panic!("Failed to compile shader: {}", err_msg.unwrap_or_else(|| String::from("Unknown error")))
+    fs::write(&format!("{}/{}", out_dir, shader_name), data).map_err(Into::into)
 }
